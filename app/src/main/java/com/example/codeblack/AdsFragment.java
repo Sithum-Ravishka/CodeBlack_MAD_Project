78081f6c006d4efac5d79581a7ce327692cc2ec3
KavindduShamalka <kavindusuraweera27@gmail.com>
2021-05-08 18:08:13 +0530

    advertisement search function

--- app/src/main/java/com/example/codeblack/AdsFragment.java
+++ app/src/main/java/com/example/codeblack/AdsFragment.java
@@ -2,63 +2,90 @@ package com.example.codeblack;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link AdsFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class AdsFragment extends Fragment {
+    RecyclerView recyclerView;
+    AdepterAdds adepterAdds;
+    List<modelAdd> addList;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+
 
     public AdsFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment AdsFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static AdsFragment newInstance(String param1, String param2) {
-        AdsFragment fragment = new AdsFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_ads, container, false);
+        View view = inflater.inflate(R.layout.row_add, container, false);
+
+        //init recycler view
+        recyclerView = view.findViewById(R.id.adds_recyclerView);
+
+        //set it's properties
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        //init add list
+        addList = new ArrayList<>();
+
+        //get all adds
+        getAllAdds();
+        return view;
+    }
+
+    private void getAllAdds() {
+
+        FirebaseUser fAdd = FirebaseAuth.getInstance().getCurrentUser();
+        //get path of database named add containing data info
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("Ads");
+        //get all data from path
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                addList.clear();
+                for(DataSnapshot ds : snapshot.getChildren()){
+                    modelAdd modelAdd = ds.getValue(com.example.codeblack.modelAdd.class);
+
+                    //get all users accept currently signed in user
+                    if(!modelAdd.getUid().equals(fAdd.getUid())){
+                        addList.add(modelAdd);
+
+                    }
+                    //adepter
+                    adepterAdds = new AdepterAdds(getActivity(), addList);
+                    //set adepter to recycler view
+                    recyclerView.setAdapter(adepterAdds);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/codeblack/modelAdd.java
@@ -0,0 +1,85 @@
+package com.example.codeblack;
+
+public class modelAdd {
+
+    //
+    String pTime,pDesPrice,pBathroom,pKitchen,pTitle,pImage,pState,uid;
+
+    public modelAdd() {
+    }
+
+    public modelAdd(String pTime, String pDesPrice, String pBathroom, String pKitchen, String pTitle,String pImage,String pState,String uid) {
+        this.pTime = pTime;
+        this.pDesPrice = pDesPrice;
+        this.pBathroom = pBathroom;
+        this.pKitchen = pKitchen;
+        this.pTitle = pTitle;
+        this.pImage = pImage;
+        this.pState  = pState;
+        this.uid = uid;
+    }
+
+    public String getpTime() {
+        return pTime;
+    }
+
+    public void setpTime(String pTime) {
+        this.pTime = pTime;
+    }
+
+    public String getpDesPrice() {
+        return pDesPrice;
+    }
+
+    public void setpDesPrice(String pDesPrice) {
+        this.pDesPrice = pDesPrice;
+    }
+
+    public String getpBathroom() {
+        return pBathroom;
+    }
+
+    public void setpBathroom(String pBathroom) {
+        this.pBathroom = pBathroom;
+    }
+
+    public String getpKitchen() {
+        return pKitchen;
+    }
+
+    public void setpKitchen(String pKitchen) {
+        this.pKitchen = pKitchen;
+    }
+
+    public String getpTitle() {
+        return pTitle;
+    }
+
+    public void setpTitle(String pTitle) {
+        this.pTitle = pTitle;
+    }
+
+    public String getpImage() {
+        return pImage;
+    }
+
+    public void setpImage(String pImage) {
+        this.pImage = pImage;
+    }
+
+    public String getpState() {
+        return pState;
+    }
+
+    public void setpState(String pState) {
+        this.pState = pState;
+    }
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+}
